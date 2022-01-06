4d13cdb3235b5300973eed3597bedb1ac6500724
takashi247 <takashi247@gmail.com>
2022-01-06 19:15:17 +0900

    add basic files for ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,19 @@
+#include "account.hpp"
+
+Account::Account(int initial_deposit):{}
+
+Account::~Account()
+{
+
+}
+
+/*
+// output example: 
+// [19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+*/
+
+static void
+    Account::displayAccountsInfos()
+{
+
+}
