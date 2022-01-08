f7dbaea0b3d65a1c33435ae9f2c49a4e1b51c3b7
takashi247 <takashi247@gmail.com>
2022-01-08 11:49:02 +0900

    fix const directives

--- ex01/include/contact.hpp
+++ ex01/include/contact.hpp
@@ -8,16 +8,16 @@ class Contact
 	public:
 		Contact();
 		~Contact();
-		const std::string	get_first_name();
-		const std::string	get_last_name();
-		const std::string	get_nickname();
-		const std::string	get_phone_number();
-		const std::string	get_darkest_secret();
-		void				set_first_name(std::string string);
-		void				set_last_name(std::string string);
-		void				set_nickname(std::string string);
-		void				set_phone_number(std::string string);
-		void				set_darkest_secret(std::string string);
+		std::string	get_first_name() const;
+		std::string	get_last_name() const;
+		std::string	get_nickname() const;
+		std::string	get_phone_number() const;
+		std::string	get_darkest_secret() const;
+		void		set_first_name(std::string string);
+		void		set_last_name(std::string string);
+		void		set_nickname(std::string string);
+		void		set_phone_number(std::string string);
+		void		set_darkest_secret(std::string string);
 	private:
 		std::string	first_name;
 		std::string last_name;
--- ex01/srcs/contact.cpp
+++ ex01/srcs/contact.cpp
@@ -7,32 +7,32 @@ Contact::Contact():
 
 Contact::~Contact(){}
 
-const std::string
-	Contact::get_first_name()
+std::string
+	Contact::get_first_name() const
 {
 	return (this->first_name);
 }
 
-const std::string
-	Contact::get_last_name()
+std::string
+	Contact::get_last_name() const
 {
 	return (this->last_name);
 }
 
-const std::string
-	Contact::get_nickname()
+std::string
+	Contact::get_nickname() const
 {
 	return (this->nickname);
 }
 
-const std::string
-	Contact::get_phone_number()
+std::string
+	Contact::get_phone_number() const
 {
 	return (this->phone_number);
 }
 
-const std::string
-	Contact::get_darkest_secret()
+std::string
+	Contact::get_darkest_secret() const
 {
 	return (this->darkest_secret);
 }
