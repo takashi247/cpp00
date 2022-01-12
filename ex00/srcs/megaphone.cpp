0e89442eeb301496907e9b2b2c0d1cae4fcf2865
takashi247 <takashi247@gmail.com>
2022-01-12 22:50:34 +0900

    add static cast to unsigned char to avoid undefined behavior of toupper

--- ex00/srcs/megaphone.cpp
+++ ex00/srcs/megaphone.cpp
@@ -14,7 +14,7 @@ int
 		{
 			std::string s = av[i];
 			for (std::string::size_type j = 0; j < s.length(); j++)
-				s[j] = std::toupper(s[j]);
+				s[j] = static_cast<char>(std::toupper(static_cast<unsigned char>(s[j])));
 			std::cout << s;
 		}
 		std::cout << std::endl;
